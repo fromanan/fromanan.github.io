d049cd0f5c10054c8c95d698019d9e9299a2cfec
fromanan@msu.edu <33915703+fromanan@users.noreply.github.com>
2019-01-18 15:07:59 -0500

    Revert "Omega Starter Pack (Replaced Wordpress)"
    This reverts commit 28a2c13db1d104d52ed6513b19d2076739f5fd88.

--- partials/global-bbpress/global-page-top-fullwidth.php
+++
@@ -1,5 +0,0 @@
-<section class="section <?php echo oxy_get_option('bbpress_swatch') ?>">
-    <div class="container">
-        <div class="row element-normal-top">
-            <div class="col-md-8 col-md-offset-2">
-
